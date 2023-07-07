9ee6bd37956ce7b8738f50f7e340c7c66f370bd5
Zlatko33Kamenov <zlatko_kamenov@abv.bg>
2023-07-07 15:52:09 +0300

    my first commit

---
+++ TEST1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TEST1.Models;
+
+namespace TEST1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public double GetCurrency(double currencyBG)
+        {
+            var currencyUSD = 1.80;
+            var result = currencyBG * currencyUSD;
+            return result;
+        }
+
+        [HttpPost]
+        public Currency Currency(Currency model)
+        {
+            
+            return model;
+        }
+    }
+}
---
+++ TEST1/Models/Currency.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TEST1.Models
+{
+    public class Currency
+    {
+        public string CurrencyName { get; set; }
+        public double CurrencyValue { get; set; }
+    }
+}
