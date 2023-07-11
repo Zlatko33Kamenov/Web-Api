c1c5b77349a448a7038417d0c3999892628b9428
Zlatko33Kamenov <138592118+Zlatko33Kamenov@users.noreply.github.com>
2023-07-11 17:19:18 +0300

    Pull request (#1)
    * Add Crud Operations
    
    * Add Solution

---
+++ tsak 1/Models/PeopleInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace tsak_1.Models
+{
+    public class PeopleInfo
+    {
+        public string FirstName { get; set; }
+        public uint Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
